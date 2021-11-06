306f2aa931fd45b7200621575316e7a6b048a90c
Istvan <harasztii@hobre.com>
2021-11-06 15:50:11 +0100

    Navbar added

--- fonixfitness/src/App.js
+++ fonixfitness/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/layout/Navbar';
 
 const App = () => {
   return (
-    <h2>Főnix Fitness</h2>
+    <>
+      <Navbar />
+      <h2>Főnix Fitness</h2>
+    </>
   )
 }
 
